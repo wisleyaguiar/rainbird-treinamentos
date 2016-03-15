feab69a89973485a5d34bab54848888d73fdbc0b
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-15 08:09:25 -0300

    Versão de Produção. 10

--- js/formularios.js
+++ js/formularios.js
@@ -4,9 +4,34 @@
 (function($) {
 
     // Cadastro de Usuários
+
+    $('#cep').mask('00000-000');
+
+    var SPMaskBehavior = function (val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+        },
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+
+    $('#cel').mask(SPMaskBehavior, spOptions);
+    $('#telfixo').mask('(00) 0000-0000');
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+
+    $.validator.addMethod("noSpace", function(value, element) {
+        return value.indexOf(" ") < 0 && value != "";
+    }, "No space please and don't leave it empty");
+
     $("#caixa-cadastro").validate({lang: 'pt_BR'});
     $("#formCadastroCompleto").validate({
         lang: 'pt_BR',
+        rules: {
+            nome_user: {
+                noSpace: true
+            }
+        },
         submitHandler: function(form) {
             if($('#sexo_1').is(":checked")) {
                 var sexo = 'M';
