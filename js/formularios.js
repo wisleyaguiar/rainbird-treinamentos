085bae327454009ddb8513da033911daa19261bf
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-15 19:21:14 -0300

    Versão de Produção. 16

--- js/formularios.js
+++ js/formularios.js
@@ -5,7 +5,7 @@
 
     // Cadastro de Usuários
 
-    $('#cep').mask('00000-000');
+    $('#cep').mask('00.000-000');
 
     var SPMaskBehavior = function (val) {
             return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
