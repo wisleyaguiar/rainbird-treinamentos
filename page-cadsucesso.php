4fb5a4e586fa690c9e69a4e81ab0ca07b6c084fd
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-22 07:55:57 -0300

    Versão de Produção. 26

--- page-cadsucesso.php
+++ page-cadsucesso.php
@@ -4,7 +4,7 @@
  */
 get_header(); ?>
 
-<div class="content" style="overflow:hidden; background:url(<?php echo get_template_directory_uri(); ?>'/images/treinamentos/bg-tela-conclusao.jpg') no-repeat 0 0; height: 552px;">
+<div class="content" style="overflow:hidden; background:url('<?php echo get_template_directory_uri(); ?>/images/treinamentos/bg-tela-conclusao.jpg') no-repeat 0 0; height: 552px;">
   <div style="width:690px; float:left; margin-left: 338px; padding-top: 30px;">
 
     <h1 style="color: #FFF; font-size: 20px; text-shadow: 2px 2px 2px #000; font-family: 'Trebuchet MS', sans-serif; margin-top: 175px">Cadastro de pessoa física - Treinamentos Rain Bird</h1>
