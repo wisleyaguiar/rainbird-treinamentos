4ae7aa36e2278b6001bf4d6fe269cb6b6a6d05fd
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-24 16:56:27 -0300

    Versão de Produção. 56

--- functions.php
+++ functions.php
@@ -74,6 +74,25 @@ add_editor_style(get_template_directory_uri() . '/css/treinamentos.css');
 add_image_size( 'treinamentosrb-logo-size', 300, 150 );
 add_theme_support( 'custom-logo', array( 'size' => 'treinamentosrb-logo-size' ) );
 
+function remove_dashboard_widgets() {
+    global $wp_meta_boxes;
+
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+
+}
+
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
+
+
+
+
 // Autenticação função
 function custom_login($username,$password) {
     $creds = array();
