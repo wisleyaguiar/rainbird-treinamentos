d3322fb3f0381e2c61d696ce71819eec7f3b6f6e
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-24 06:09:54 -0300

    Versão de Produção. 50

--- page-modulos.php
+++ page-modulos.php
@@ -24,7 +24,7 @@ setlocale(LC_MONETARY, 'pt_BR'); ?>
       <form action="<?php echo home_url('/'); ?>" method="post" id="formEscolhaModulos">
           <?php if($tipo_curso=='tipo_modulo') { ?>
           <?php
-              $total_salas = get_post_meta($curso_is, 'total_salas', true);
+              $total_salas = get_post_meta($curso_id, 'total_salas', true);
               for($i=1;$i<$total_salas;$i++) { ?>
           <table border="0" class="tabela-comerce">
               <tbody>
