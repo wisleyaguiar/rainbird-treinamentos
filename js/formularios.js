a7ec6369c7ad80a88b6c5b184302c523e1fd424b
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-26 10:07:30 -0300

    Versão de Produção. 56

--- js/formularios.js
+++ js/formularios.js
@@ -329,7 +329,7 @@
     $('#formEscolhaModulos input[type=checkbox]').click(function(){
         var checkbox = $(this);
 
-        if(checkbox.attr('name') === 'modulo_todos') {
+        if(checkbox.is(':checked') && checkbox.attr('name') === 'modulo_todos') {
             $('#sala-' + checkbox.val() + ' input[type=checkbox]').attr('checked',false).attr('disabled',true);
             checkbox.attr('checked',true).attr('disabled',false);
         }
