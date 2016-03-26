02493c6cefc91dd96a574f5bc475d97fecc9f9c8
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-26 10:13:24 -0300

    Versão de Produção. 56

--- js/formularios.js
+++ js/formularios.js
@@ -334,6 +334,9 @@
             $('input[type=radio]').attr("disabled",true).attr("checked",false);
             checkbox.attr('checked',true).attr('disabled',false);
         }
+        else if(checkbox.is(':checked') && checkbox.attr('name') != 'modulo_todos') {
+            $('input[name=modulo-' + checkbox.val() + '-valor]').attr("disabled",false);
+        }
 
         /*if(checkbox.is(':checked')) {
             $('input[name=' + checkbox.attr('id') + '-hosp]').attr("disabled",false);
