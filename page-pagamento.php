48d484f77276a4f88f152807fdbe48a49953e3cc
wisleyaguiar <wisleyaguiar@hotmail.com>
2016-03-28 20:29:37 -0300

    Versão de Produção. 66

--- page-pagamento.php
+++ page-pagamento.php
@@ -23,12 +23,14 @@ setlocale(LC_MONETARY, 'pt_BR');
     ?>
       <p><strong style="font-size: 15px; color: #10724c">Treinamento escolhido:</strong><br><?php echo get_post_meta($id_inscricao,'ins_nome_curso',true); ?></p>
 
-      <p><strong style="font-size: 15px;color: #10724c">Módulos Selecionados:</strong></p>
+      <p><strong style="font-size: 15px;color: #10724c">Módulos Selecionados:</strong><br>
 
       <?php $cursos = get_post_meta($id_inscricao,'ins_id_modulos_curso',true);
             $valores = get_post_meta($id_inscricao,'ins_valores_curso',true); ?>
 
-      <?php print_r($cursos); ?>
+      <?php foreach ($cursos as $curso) { ?>
+          <?php echo get_post_meta($curso,'cod_modulo', true); ?> - <?php echo get_post($curso)->post_title; ?><br>
+      <?php } ?></p>
 
       <p><strong style="font-size: 15px;color: #10724c">Total da sua compra:</strong><br>
           <?php echo money_format('%.2n', get_post_meta($id_inscricao,'ins_total_pagamento',true)) ?>
